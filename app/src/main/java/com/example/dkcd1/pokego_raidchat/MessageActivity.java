aae2d334157cfb7bf37c143d9540b568ac7611bf
evistix <denzk6@gmail.com>
2019-07-26 12:53:11 +1200

    Got phone to communicate with server

--- app/src/main/java/com/example/dkcd1/pokego_raidchat/MessageActivity.java
+++ app/src/main/java/com/example/dkcd1/pokego_raidchat/MessageActivity.java
@@ -1,6 +1,11 @@
 package com.example.dkcd1.pokego_raidchat;
 
 import android.support.v7.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.io.DataOutputStream;
@@ -41,53 +46,55 @@ public class MessageActivity extends AppCompatActivity {
                 TextView v = findViewById(R.id.user_message);
                 v.setText(msg);
 
-                //client(msg);
+                client(msg);
             }
         });
     }
 
-    public void client(String msg) {
-        String hostName = "cms-r1-18";
-        int portNumber = 8080;
+    public void client(final String text) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //String hostName = "130.217.252.16";
+                String hostName = "130.217.252.18";
+                int portNumber = 8080;
+                System.out.println("2");
 
-        // establish socket connection to server
-        Socket socket;
-        try {
-            socket = new Socket(hostName, portNumber);
-        } catch (UnknownHostException e) {
-            System.out.println("Couldn't establish socket connection");
-            return;
-        } catch (IOException e) {
-            System.out.println("IO exception on attempting to connect socket");
-            return;
-        }
-
-        // write to socket using OutputStream
-        try {
-            String keyboardInput;
-            DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-            //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            // Tell client how to end
-            System.out.print( "Connection made; type the word END to end the connection\n");
-
-            while(true){
-                // Read input form keyboard
-                keyboardInput = "test";
-
-                // Allow the client to end the connection
-                if (keyboardInput.equals("END")){
-                    System.out.print("Ending connection\n");
-                    break;
+                // establish socket connection to server
+                Socket socket;
+                try {
+                    socket = new Socket(hostName, portNumber);
+                } catch (UnknownHostException e) {
+                    System.out.println("Couldn't establish socket connection");
+                    return;
+                } catch (IOException e) {
+                    System.out.println("IO exception on attempting to connect socket");
+                    return;
+                } catch (Exception e) {
+                    System.out.println(e);
+                    return;
                 }
 
-                // Send the message
-                os.writeBytes(keyboardInput + "\n");
+                // write to socket using OutputStream
+                try {
+                    System.out.println("1");
+
+                    // Tell client how to end
+                    System.out.print("Connection made; type the word END to end the connection\n");
+
+                    OutputStream out = socket.getOutputStream();
+                    PrintWriter output = new PrintWriter(out);
+
+                    output.println(text);
+                    output.flush();
+                    out.close();
+                } catch (IOException e) {
+                    System.out.println("IO exception trying to write to socket");
+                    return;
+                }
             }
-            os.close();
-        } catch (IOException e) {
-            System.out.println("IO exception trying to write to socket");
-            return;
-        }
+        });
+        t.start();
     }
+
 }
