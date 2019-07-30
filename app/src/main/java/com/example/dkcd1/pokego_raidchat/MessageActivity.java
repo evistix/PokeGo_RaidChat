6bdb93bde8823c13f811248fb2b087aeeef1a129
evistix <denzk6@gmail.com>
2019-07-30 19:34:57 +1200

    Fixed client side closing connection after sending message. Need to fix socket closing while app still open. Need to be able to receive from server

--- app/src/main/java/com/example/dkcd1/pokego_raidchat/MessageActivity.java
+++ app/src/main/java/com/example/dkcd1/pokego_raidchat/MessageActivity.java
@@ -1,5 +1,6 @@
 package com.example.dkcd1.pokego_raidchat;
 
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 
 import java.io.BufferedReader;
@@ -8,11 +9,14 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -20,9 +24,20 @@ import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
 
+    final static String END_KEY = "end_con";
+    Thread t;
+    boolean connected = false;
+    String userId= "Paul";
+    Socket[] socket = new Socket[1];
+    OutputStream out;
+    PrintWriter output;
+    String msg;
+
     ImageButton btn_send;
+    Button btn_connect;
     EditText text_send;
     LinearLayout place_holder;
+    List messages = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,38 +47,53 @@ public class MessageActivity extends AppCompatActivity {
         btn_send = findViewById(R.id.btn_send);
         text_send = findViewById(R.id.text_send);
         place_holder = findViewById(R.id.place_holder);
+        server_connect();
 
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String msg = text_send.getText().toString();
+                msg = text_send.getText().toString();
                 System.out.println(msg);
-
 
                 text_send.setText("");
 
-                getLayoutInflater().inflate(R.layout.chat_item_right,place_holder);
-                TextView v = findViewById(R.id.user_message);
-                v.setText(msg);
+                server_connect();
+                //Display all messages
 
-                client(msg);
+                place_holder.removeAllViews();
+                for(int i = messages.size() - 1; i >= 0; i--) {
+                    View s = getLayoutInflater().inflate(R.layout.chat_item_right, null);
+                    TextView v = s.findViewById(R.id.user_message);
+                    v.setText(messages.get(i).toString());
+                    System.out.println(messages.get(i).toString());
+                    if(messages.get(i).toString() != "") {
+                        place_holder.addView(s);
+                    }
+                }
+                System.out.println(socket[0]);
             }
         });
     }
 
-    public void client(final String text) {
-        Thread t = new Thread(new Runnable() {
+    public void server_connect () {
+
+        t = new Thread(new Runnable() {
             @Override
             public void run() {
                 //String hostName = "130.217.252.16";
-                String hostName = "130.217.252.18";
-                int portNumber = 8080;
-                System.out.println("2");
+                String hostName = "130.217.243.90";
+                int portNumber = 6050;
 
                 // establish socket connection to server
-                Socket socket;
                 try {
-                    socket = new Socket(hostName, portNumber);
+                    if(!connected) {
+                        socket[0] = new Socket(hostName, portNumber);
+                        out = socket[0].getOutputStream();
+                        output = new PrintWriter(out);
+                        output.println(userId);
+                        messages.add("");
+                        connected = true;
+                    }
                 } catch (UnknownHostException e) {
                     System.out.println("Couldn't establish socket connection");
                     return;
@@ -75,26 +105,38 @@ public class MessageActivity extends AppCompatActivity {
                     return;
                 }
 
-                // write to socket using OutputStream
-                try {
-                    System.out.println("1");
+                while(t.isAlive() && socket != null) {
+                    // write to socket using OutputStream
+                    try {
+                        System.out.println("1");
 
-                    // Tell client how to end
-                    System.out.print("Connection made; type the word END to end the connection\n");
+                        // Tell client how to end
+                        if(!connected)
+                        System.out.print("Connection made; type the word 'end_con' to end the connection\n");
 
-                    OutputStream out = socket.getOutputStream();
-                    PrintWriter output = new PrintWriter(out);
-
-                    output.println(text);
-                    output.flush();
-                    out.close();
-                } catch (IOException e) {
-                    System.out.println("IO exception trying to write to socket");
-                    return;
+                        while(true) {
+                            if(msg == END_KEY) {
+                                break;
+                            }
+                            if(msg != "" && msg != null) {
+                                output.println(msg);
+                                output.flush();
+                                messages.add(msg);
+                                msg = "";
+                            }
+                        }
+                        out.close();
+                    } catch (IOException e) {
+                        System.out.println("IO exception trying to write to socket");
+                        return;
+                    }
                 }
             }
         });
         t.start();
+        if(!t.isAlive()) {
+            connected = false;
+        }
     }
 
 }
